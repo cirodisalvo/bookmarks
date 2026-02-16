87b5e639773b505a8e63b8fd6a3bab38002c3b1a
cirodisalvo <ciro.disalvo+github@gmail.com>
2026-02-16 17:25:52 +0100

    Add files via upload

---
+++ links.js
@@ -0,0 +1,214 @@
+const links = `
+# Personale 🧑‍🦲
+
+## Ricerca
+- [DuckDuckGo](https://duckduckgo.com/)
+- [Startpage](https://www.startpage.com/)
+- [Google](https://www.google.com/)
+- [Yahoo](https://it.search.yahoo.com/)
+
+## Google
+- [Google My Account](https://myaccount.google.com/)
+- [YouTube](https://www.youtube.com/)
+- [Google Keep](https://keep.google.com/)
+- [Google Drive](https://drive.google.com/)
+- [Google Calendar](https://calendar.google.com/)
+- [Google Maps](https://www.google.com/maps)
+- [Google Meet](https://meet.google.com/)
+- [Google Translate](https://translate.google.com/)
+- [Google Contacts](https://contacts.google.com/)
+
+## Cloud
+- [GDrive](https://drive.google.com/drive/my-drive)
+- [Pcloud](https://www.pcloud.com/it/)
+- [pCloud share](http://tinyurl.com/share-pcloud-ciro)
+- [Microsoft OneDrive](https://onedrive.live.com/login)
+
+## Email
+- [Gmail](https://mail.google.com/mail/u/0/#inbox)
+- [Outlook](https://outlook.live.com/mail/0/)
+- [Yahoo](https://login.yahoo.com/)
+- [Pec mail](https://webmail.pec.it/)
+- [Domicilio digitale](https://domiciliodigitale.gov.it/dgit/home/public/#!/home)
+
+## Shop
+- [UniCredit Banca](https://www.unicredit.it/it/privati.html)
+- [Amazon](https://www.amazon.it/)
+- [Cotton&Silk](https://www.cottonsilk.it/it)
+- [Privalia](https://it.privalia.com/gr/home/default)
+- [iliad](https://www.iliad.it/account/)
+- [ho. Mobile](https://www.ho-mobile.it/login.html)
+- [PayPal](https://www.paypal.com/it/signin)
+- [Reinold's Fashion SGG](https://www.reinolds.it/web/)
+
+## IA
+- [Google Gemini](https://gemini.google.com/app)
+- [Claude](https://claude.ai/)
+- [ChatGPT](https://chatgpt.com/)
+- [Perplexity](https://www.perplexity.ai/)
+- [DeepSeek](https://chat.deepseek.com/)
+- [Qwen Chat](https://chat.qwen.ai/)
+- [Z.ai Chat](https://chat.z.ai/)
+
+## Notizie
+- [Ansa](https://www.ansa.it/)
+- [Il Post](https://www.ilpost.it/)
+- [la Repubblica](https://www.repubblica.it/)
+- [L'Indipendente](https://www.lindipendente.online/)
+
+---
+
+# Scuola 🏫
+
+## Google scuola
+- [Google](https://www.google.com/)
+- [Classroom](https://classroom.google.com/)
+- [Google Meet](https://meet.google.com/)
+
+## Impegni
+- [Tasks](https://tasks.google.com/tasks/)
+- [Google Calendar](https://calendar.google.com/)
+
+## Registro elettronico
+- [Argo didUP](https://www.portaleargo.it/voti/)
+- [Classeviva](https://web.spaggiari.eu/home/app/default/login.php)
+
+## Email scuola
+- [Outlook scuola](https://outlook.live.com/mail/0/)
+- [Pec email](https://webmail.pec.it/)
+- [Domicilio digitale](https://domiciliodigitale.gov.it/dgit/home/public/#!/home)
+
+## Notizie Scuola
+- [Normativa Inclusione](https://www.normativainclusione.it/)
+- [Orizzonte Scuola](https://www.orizzontescuola.it/)
+- [Tecnica della Scuola](https://www.tecnicadellascuola.it/)
+- [Obiettivo Scuola](https://www.obiettivoscuola.it/)
+- [Tuttoscuola](https://www.tuttoscuola.com/)
+- [USR Sicilia](https://ag.usr.sicilia.it/tutte-le-news/)
+
+## Ministero Istruzione
+- [MIM](https://www.mim.gov.it/)
+- [Area riservata MIM](https://iam.pubblica.istruzione.it/iam-areariservata-web)
+- [Carta del Docente](https://www.cartadeldocente.istruzione.it/)
+- [Indire Inclusione](https://inclusione.indire.it/)
+- [Istanze OnLine](https://www.istruzione.it/polis/Istanzeonline.htm)
+- [Fascicolo Digitale](https://fascicolodigitale.istruzione.gov.it/)
+- [S.O.F.I.A.](https://sofia.istruzione.it/)
+- [Scuola futura](https://scuolafutura.pubblica.istruzione.it/)
+- [NoiPA](https://noipa.mef.gov.it/cl/web/guest/home)
+
+## Università
+- [Pegaso](https://lms.pegaso.multiversity.click/)
+- [Portale ANIS](https://www.anis.mur.gov.it/)
+
+## IA Scuola
+- [MagicSchool AI](https://app.magicschool.ai/tools)
+- [Teachable Machine](https://teachablemachine.withgoogle.com/)
+
+---
+
+# Italiano 🇮🇹
+
+## Sinonimi
+- [Dizy Sinonimi](https://www.dizy.com/it/sinonimi/)
+- [Sinonimi-Contrari](https://www.sinonimi-contrari.it/)
+- [Virgilio Sapere](https://sapere.virgilio.it/parole/sinonimi-e-contrari/)
+
+## Dizionario
+- [Dizy](https://www.dizy.com/)
+- [Treccani](https://www.treccani.it/vocabolario/)
+- [Dizionario Repubblica](https://dizionari.repubblica.it/italiano.html)
+- [Sapere](https://www.sapere.it/sapere/dizionari.html)
+- [De Mauro](https://dizionario.internazionale.it/)
+
+## Grammatica
+- [Grammatica pratica](https://grammatica-italiana.dossier.net/)
+- [La Grammatica Italiana](https://www.lagrammaticaitaliana.it/)
+
+## Coniugazione
+- [Coniugazione](https://www.coniugazione.it/)
+- [Italian-Verbs](https://www.italian-verbs.com/verbi-italiani.php)
+- [Reverso](https://coniugazione.reverso.net/coniugazione-italiano.html)
+
+---
+
+# Libri 📚
+
+## Kindle
+- [Invia a Kindle](https://www.amazon.it/sendtokindle)
+- [Gestione dispositivi](https://www.amazon.it/hz/mycd/digital-console/alldevices)
+- [Kindle Library](https://read.amazon.com/kindle-library)
+
+## Epub
+- [PDF to EPUB](https://www.epub.to/pdf-epub/)
+- [Bookize](https://www.bookize.com/it/pdf-to-epub/)
+- [ACSM Converter](https://www.acsmconverter.com/)
+- [Convertio AZW3](https://convertio.co/it/azw3-epub/)
+
+## Librerie
+- [IBS](https://www.ibs.it/)
+- [Feltrinelli](https://www.lafeltrinelli.it/)
+- [Mondadori](https://www.mondadoristore.it/)
+- [HOEPLI](https://www.hoepli.it/)
+- [Project Gutenberg](https://www.gutenberg.org/)
+
+---
+
+# APP 💽
+
+## PC
+- [IPLeak](https://ipleak.net/)
+- [VirusTotal](https://www.virustotal.com/gui/home/upload)
+- [Speedtest](https://www.speedtest.net/it)
+- [Have I Been Pwned](https://haveibeenpwned.com/)
+- [Intelligence X](https://intelx.io/)
+- [XPPen Drivers](https://www.xp-pen.com/download/star-g640.html)
+
+## PortableApp
+- [Flathub](https://flathub.org/it)
+- [AppImages hub](https://appimage.github.io/)
+- [PortableApps](https://portableapps.com/apps)
+
+## Linux
+- [KOReader](https://github.com/koreader/koreader/releases)
+- [Ventoy](https://github.com/ventoy/Ventoy/releases)
+
+## Browser
+- [MyDashboard](https://docs.google.com/spreadsheets/d/1zIyLXhIfn6mzYwRjn9ArNbyaPD1waH7IGa0Z0ExPUtE/edit?gid=1805985440#gid=1805985440)
+- [Addons Firefox](https://addons.mozilla.org/it/firefox/)
+- [Centro sviluppo](https://addons.mozilla.org/it/developers/addons)
+
+## PDF Tools
+- [iLovePDF](https://www.ilovepdf.com/it)
+- [PDF Candy](https://pdfcandy.com/it/)
+- [PDF24](https://tools.pdf24.org/it/)
+- [Smallpdf](https://smallpdf.com/it)
+- [PDF24 Immagini](https://tools.pdf24.org/it/immagini-in-pdf)
+- [Convert JPG to PDF](https://www.convert-jpg-to-pdf.net/)
+- [PDF a DOC](https://pdf2doc.com/it/)
+
+## Image Tools
+- [JS Paint](https://jspaint.app/)
+- [iLoveIMG](https://www.iloveimg.com/)
+- [Remove Background](https://www.remove.bg/it/upload)
+- [Disegno Matita](https://www.fotoeffetti.com/cat-modificare-foto/convertire-picture-disegno-a-matita)
+- [Resize Image](https://www.birme.net)
+
+## YouTube Tools
+- [YouTube Subtitles](https://www.dvdvideosoft.com/online-youtube-subtitles-download)
+- [you-tldr](https://www.you-tldr.com/)
+- [SSYouTube](https://ssyoutube.com/en811KJ/)
+
+## Converter
+- [Online converter](https://www.online-convert.com/)
+- [MP3Toobox](https://mp3toolbox.net/)
+- [GB to MB](https://www.convertunits.com/from/GB/to/MB)
+- [CloudConvert](https://cloudconvert.com/)
+
+## Text Tools
+- [Replace text](https://www.unit-conversion.info/texttools/replace-text/#data)
+- [Add prefix](https://www.unit-conversion.info/texttools/add-prefix-into-line/#data)
+- [CSV To HTML](https://www.convertcsv.com/csv-to-html.htm)
+- [HTML5 Editor](https://html5-editor.net/)
+
+`;
